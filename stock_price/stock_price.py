79e0b1e12806e7d722e116b84ae796eaa3905bfe
yccheok <yancheng.cheok@gmail.com>
2018-02-06 20:47:23 +0800

    flower.

--- stock_price/stock_price.py
+++ stock_price/stock_price.py
@@ -3,8 +3,8 @@ from celery import Celery
 
 app = Celery('stock_price')
 
-@app.task(name='stock_price.mul')
-def mul(x, y):
+@app.task(name='stock_price.mul', bind=True)
+def mul(self, x, y):
     time.sleep(5)
     timestamp = time.time()
     print("stock_price -> " + str(timestamp))
